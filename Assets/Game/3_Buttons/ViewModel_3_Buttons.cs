6b14929155b360d702ff0024090305f2f11f2eea
Gabriel Dechichi <gabrieldechichi@gmail.com>
2020-12-30 13:46:26 -0700

    Basic Event Binding example with Button

---
+++ Assets/Game/3_Buttons/ViewModel_3_Buttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld.Binding;
+
+[Binding]
+public class ViewModel_3_Buttons : ViewModel
+{
+    [SerializeField] Transform cube = default;
+    [SerializeField] float rotation = 90;
+
+    [Binding]
+    public void RotateCube()
+    {
+        cube.Rotate(cube.up, rotation);
+    }
+}
