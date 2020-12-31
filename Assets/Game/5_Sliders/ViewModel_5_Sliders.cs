5d9be75f7d92bfb9e83e375dc79fbc8f23fef673
Gabriel Dechichi <gabrieldechichi@gmail.com>
2020-12-30 20:41:37 -0700

    5 - Sliders + Multiple two way binding

---
+++ Assets/Game/5_Sliders/ViewModel_5_Sliders.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld.Binding;
+
+[Binding]
+public class ViewModel_5_Sliders : ViewModel
+{
+    private float sliderValue = 2.5f;
+
+    private bool isValid = true;
+
+    [SerializeField] private Transform cube;
+
+    [Binding]
+    public float SliderValue
+    {
+        get => sliderValue;
+        set 
+        { 
+            if (sliderValue != value) 
+            { 
+                sliderValue = value; 
+                UpdateCubeRotation(); 
+                OnPropertyChanged(nameof(SliderValue)); 
+            } 
+        }
+    }
+
+    [Binding]
+    public bool IsValid
+    {
+        get => isValid;
+        set { if (isValid != value) { isValid = value; OnPropertyChanged(nameof(IsValid)); } }
+    }
+
+    private void Start()
+    {
+        UpdateCubeRotation();
+    }
+
+    private void UpdateCubeRotation()
+    {
+        cube.rotation = Quaternion.Euler(0f, SliderValue, 0f);
+    }
+}
