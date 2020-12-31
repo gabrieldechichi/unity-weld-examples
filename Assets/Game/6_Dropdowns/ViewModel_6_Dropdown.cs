b9f902a43b052da499ca197b2e4ab3d009b6ecda
Gabriel Dechichi <gabrieldechichi@gmail.com>
2020-12-30 20:53:36 -0700

    Dropdown

---
+++ Assets/Game/6_Dropdowns/ViewModel_6_Dropdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld.Binding;
+
+[Binding]
+public class ViewModel_6_Dropdown : ViewModel
+{
+    private static string[] options = new string[]
+    {
+        "Option1",
+        "Option2",
+        "Option3"
+    };
+
+    private string selectedItem = options[0];
+
+    [Binding]
+    public string[] Options { get => options; }
+
+    [Binding]
+    public string SelectedItem
+    {
+        get => selectedItem;
+        set
+        {
+            if (selectedItem != value)
+            {
+                selectedItem = value;
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+    }
+}
