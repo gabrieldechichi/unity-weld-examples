645ee3ec776c09af4f3a3bf63e0a5fa358a839a8
Gabriel <gabrieldechichi@gmail.com>
2020-12-31 14:11:47 -0700

    9 - Subviews

---
+++ Assets/Game/9_Subview/ColorControlView.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityWeld.Binding;
+
+[Binding]
+public class ColorControlView
+{
+    private Color color = default;
+    private MeshRenderer cube;
+
+    public ColorControlView(MeshRenderer cube)
+    {
+        this.cube = cube;
+        OnColorChanged();
+    }
+
+    [Binding]
+    public float Red
+    {
+        get => color.r;
+        set
+        {
+            if (Red != value)
+            {
+                color.r = value;
+                OnColorChanged();
+            }
+        }
+    }
+
+    [Binding]
+    public float Green
+    {
+        get => color.g;
+        set
+        {
+            if (Green != value)
+            {
+                color.g = value;
+                OnColorChanged();
+            }
+        }
+    }
+
+    [Binding]
+    public float Blue
+    {
+        get => color.b;
+        set
+        {
+            if (Blue != value)
+            {
+                color.b = value;
+                OnColorChanged();
+            }
+        }
+    }
+
+    private void OnColorChanged()
+    {
+        cube.material.color = color;
+    }
+}
---
+++ Assets/Game/9_Subview/ViewModel_9_Subview.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityWeld.Binding;
+
+[Binding]
+public class ViewModel_9_Subview : ViewModel
+{
+    [SerializeField] private GameObject cube = null;
+
+    private float rotation;
+
+    private ColorControlView colorView = default;
+
+    [Binding]
+    public float Rotation
+    {
+        get => rotation;
+        set
+        {
+            if (rotation != value)
+            {
+                rotation = value;
+                cube.transform.rotation = Quaternion.Euler(0, Rotation, 0);
+                OnPropertyChanged(nameof(Rotation));
+            }
+        }
+    }
+
+    [Binding]
+    public ColorControlView ColorView 
+    { 
+        get
+        {
+            if (colorView == null)
+            {
+                colorView = new ColorControlView(cube.GetComponent<MeshRenderer>());
+            }
+            return colorView;
+        }
+    }
+
+    private void Awake()
+    {
+        Assert.IsNotNull(cube);
+        Assert.IsNotNull(colorView);
+        Rotation = 0;
+    }
+}
